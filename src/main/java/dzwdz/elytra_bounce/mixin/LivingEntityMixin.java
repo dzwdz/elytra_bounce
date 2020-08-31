89a07440852b8109e9232e0596668af23a762434
Dźwiedź <kg67199@gmail.com>
2020-08-31 08:07:08 +0200

    Consistent bounces

--- src/main/java/dzwdz/elytra_bounce/mixin/LivingEntityMixin.java
+++ src/main/java/dzwdz/elytra_bounce/mixin/LivingEntityMixin.java
@@ -9,24 +9,20 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public abstract class LivingEntityMixin {
     private int elytrabounce$timer = 0;
 
-    private void elytrabounce$hook(LivingEntity entity, boolean val) {
-        if (entity.getVelocity().y == 0) {
-            elytrabounce$timer += 1;
-            if (elytrabounce$timer > 4)
-                ((EntityAccessor)entity).callSetFlag(7, val);
-        } else {
-            elytrabounce$timer = 0;
-        }
-    }
-
     @Redirect(method = "Lnet/minecraft/entity/LivingEntity;travel(Lnet/minecraft/util/math/Vec3d;)V",
               at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;setFlag(IZ)V"))
-    public void travel(LivingEntity entity, int idx, boolean val) {
-        elytrabounce$hook(entity, val);
-    }
+    public void travel(LivingEntity entity, int idx, boolean val) { }
+
+
     @Redirect(method = "Lnet/minecraft/entity/LivingEntity;initAi()V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;setFlag(IZ)V"))
     public void initAi(LivingEntity entity, int idx, boolean val) {
-        elytrabounce$hook(entity, val);
+        if (entity.getVelocity().y == 0) {
+            if (elytrabounce$timer > 1)
+                ((EntityAccessor) entity).callSetFlag(7, val);
+            elytrabounce$timer += 1;
+        } else {
+            elytrabounce$timer = 0;
+        }
     }
 }
