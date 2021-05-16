21c717d9310362d9fc549f76bfe870ffb6f80445
AdrianTodt <adriantodt.ms@gmail.com>
2021-05-16 16:42:44 -0300

    Implementation of ElytraBounce compatible with FFL

--- src/main/java/dzwdz/elytra_bounce/EntryPoint.java
+++ src/main/java/dzwdz/elytra_bounce/EntryPoint.java
@@ -1,10 +1,23 @@
 package dzwdz.elytra_bounce;
 
+import net.adriantodt.fallflyinglib.event.StopFallFlyingCallback;
 import net.fabricmc.api.ModInitializer;
 
 public class EntryPoint implements ModInitializer {
     @Override
     public void onInitialize() {
-
+        StopFallFlyingCallback.EVENT.register((player, reason, cancel) -> {
+            if (reason == StopFallFlyingCallback.Reason.CONDITIONS_NOT_MET) {
+                ElytraBounceLivingEntity ext = (ElytraBounceLivingEntity) player;
+                if (player.getVelocity().y == 0) {
+                    if (ext.elytrabounce$getTimer() <= 1) {
+                        cancel.run();
+                    }
+                    ext.elytrabounce$incrementTimer();
+                } else {
+                    ext.elytrabounce$resetTimer();
+                }
+            }
+        });
     }
 }
--- src/main/java/dzwdz/elytra_bounce/mixin/LivingEntityMixin.java
+++ src/main/java/dzwdz/elytra_bounce/mixin/LivingEntityMixin.java
@@ -1,28 +1,34 @@
 package dzwdz.elytra_bounce.mixin;
 
+import dzwdz.elytra_bounce.ElytraBounceLivingEntity;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin {
+public abstract class LivingEntityMixin implements ElytraBounceLivingEntity {
     private int elytrabounce$timer = 0;
 
-    @Redirect(method = "Lnet/minecraft/entity/LivingEntity;travel(Lnet/minecraft/util/math/Vec3d;)V",
-              at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;setFlag(IZ)V"))
-    public void travel(LivingEntity entity, int idx, boolean val) { }
+    @Override
+    public int elytrabounce$getTimer() {
+        return elytrabounce$timer;
+    }
 
+    @Override
+    public void elytrabounce$incrementTimer() {
+        elytrabounce$timer++;
+    }
 
-    @Redirect(method = "Lnet/minecraft/entity/LivingEntity;initAi()V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;setFlag(IZ)V"))
-    public void initAi(LivingEntity entity, int idx, boolean val) {
-        if (entity.getVelocity().y == 0) {
-            if (elytrabounce$timer > 1)
-                ((EntityAccessor) entity).callSetFlag(7, val);
-            elytrabounce$timer += 1;
-        } else {
-            elytrabounce$timer = 0;
-        }
+    @Override
+    public void elytrabounce$resetTimer() {
+        elytrabounce$timer = 0;
+    }
+
+    @Redirect(
+        method = "travel(Lnet/minecraft/util/math/Vec3d;)V",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;setFlag(IZ)V")
+    )
+    public void travel(LivingEntity entity, int idx, boolean val) {
     }
 }
