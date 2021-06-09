b7fb739715b09a5051e3627f6af9aa5e25d6b459
AdrianTodt <adriantodt.ms@gmail.com>
2021-06-09 19:39:07 -0300

    Added "lastAge" not limit the timer increment to once per tick.

--- src/main/java/dzwdz/elytra_bounce/mixin/LivingEntityMixin.java
+++ src/main/java/dzwdz/elytra_bounce/mixin/LivingEntityMixin.java
@@ -1,14 +1,22 @@
 package dzwdz.elytra_bounce.mixin;
 
 import dzwdz.elytra_bounce.ElytraBounceLivingEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin implements ElytraBounceLivingEntity {
+public abstract class LivingEntityMixin extends Entity implements ElytraBounceLivingEntity {
     private int elytrabounce$timer = 0;
+    private int elytrabounce$lastAge = 0;
+
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
 
     @Override
     public int elytrabounce$getTimer() {
@@ -17,7 +25,10 @@ public abstract class LivingEntityMixin implements ElytraBounceLivingEntity {
 
     @Override
     public void elytrabounce$incrementTimer() {
-        elytrabounce$timer++;
+        if (elytrabounce$lastAge != this.age) {
+            elytrabounce$lastAge = this.age;
+            elytrabounce$timer++;
+        }
     }
 
     @Override
